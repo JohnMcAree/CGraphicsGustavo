7e0784e01c63a3035b8269765503e3905dd54f26
John McAree <jm39639@ou.ac.uk>
2025-11-28 17:55:11 +0000

    adding more rotation

--- 3drenderer/src/main.c
+++ 3drenderer/src/main.c
@@ -82,13 +82,13 @@ void update(void) {
     for (int i = 0; i < N_POINTS; i++){
 	vec3_t point = cube_points[i];
 
-    cube_rotation.x =0.01;			
-    cube_rotation.y =0.01;			
-    cube_rotation.z =0.01;			
+    cube_rotation.x += 0.01;			
+    cube_rotation.y += 0.01;			
+    cube_rotation.z += 0.01;			
     
     vec3_t transformed_point =vec3_rotate_x(point, cube_rotation.x);
     
-    point.z -=camera_position.z;
+    transformed_point.z -= camera_position.z;
 	//project the current point
 	vec2_t projected_point = project(transformed_point);
 
